352b37b423f9349e353a14abb7e390baa88a6578
iman malekian <imanmalekian31@gmail.com>
2021-07-10 21:26:02 +0430

    fix(eslint): :bug: Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,31 +4,10 @@ module.exports = {
     browser: true,
     node: true
   },
-  extends: [
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: ['prettier'],
-  rules: {
-    'vue/component-name-in-template-casing': [
-      'error',
-      'kebab-case',
-      { registeredComponentsOnly: false }
-    ],
-    'no-console': [
-      'warn',
-      {
-        allow: ['error']
-      }
-    ],
-    curly: 'error',
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto'
-      }
-    ]
-  }
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false
+  },
+  extends: ["plugin:nuxt/recommended"],
+  plugins: []
 };
